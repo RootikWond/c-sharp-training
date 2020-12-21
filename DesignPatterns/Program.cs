ed16a11fe715f47cdb4468a430dfc2fea623aae9
RootikWond <rootikwond@gmail.com>
2020-12-22 00:10:12 +0200

    bobo noob line added

--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -7,6 +7,7 @@ namespace DesignPatterns
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Bobo noob");
         }
     }
 }
